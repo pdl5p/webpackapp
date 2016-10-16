c9c50378e177bc820a022ceaf47d057f0c3988d4
pdl5p <paul.lucas@fivep.com.au>
2016-10-16 15:10:43 +1100

    test api

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,10 @@ var port = process.env.port || 8080;
 
 app.use('/', express.static(__dirname));
 
+app.get('/api', (req, res) => {
+    res.send("Hello API");
+});
+
 app.listen(port, () => {
     console.log(`Listening on: ${port}`);
 });
