31d83cfb5698bdeb66367169c4cfcdc2a3ae9f5a
pdl5p <paul.lucas@fivep.com.au>
2016-10-16 14:13:52 +1100

    HtmlWebPack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
      entry: './entry.js',
      output: {
          path: './dist',
          filename: 'bundle.js'
-     }
+     },
+     plugins: [new HtmlWebPackPlugin()]
  };
